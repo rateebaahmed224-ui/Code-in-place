fab3d5214d1a1ce00755507ceb33b6913f1b33ec
rateebaahmed224-ui <rateebaahmed224@gmail.com>
2025-09-20 21:04:58 +0500

    Add files via upload

---
+++ 3 CT-173.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() 
+{
+    int units;
+    float bill = 0;
+
+    printf("Enter total units consumed: ");
+    scanf("%d", &units);
+
+    if (units <= 100) {
+        bill = units * 5;
+    } else if (units <= 200) {
+        bill = (100 * 5) + (units - 100) * 7;
+    } else {
+        bill = (100 * 5) + (100 * 7) + (units - 200) * 10;
+    }
+
+    printf("Total electricity bill = Rs. %.2f\n", bill);
+
+    return 0;
+}
